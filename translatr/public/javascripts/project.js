0e41e01eb5d938d999080ccd1e50686389095f1a
René Samselnig <Rene.Samselnig@skiline.cc>
2016-09-18 14:39:25 +0200

    Chg: Put timeline in a separate module (closes #3)

--- translatr/public/javascripts/project.js
+++ translatr/public/javascripts/project.js
@@ -1,39 +1,34 @@
-var options = {
-	chartPadding: {
-		left: -10,
-		bottom: -10
-	},
-	showArea: true,
-	showLine: false,
-	showPoint: false,
-	stackBars: true,
-	axisX: {
-		type: Chartist.FixedScaleAxis,
-		divisor: 8,
-		labelInterpolationFnc: function(value) {
-			return moment(value).format('MMM D, HH:00');
+App.Modules.TimelineModule = function(sb) {
+	var options = {
+		chartPadding: {
+			bottom: -10
 		},
-		showGrid: false
-	},
-	axisY: {
-		onlyInteger: true,
-		showGrid: false
-	}
+		showArea: true,
+		showLine: false,
+		showPoint: false,
+		stackBars: true,
+		axisX: {
+			type: Chartist.FixedScaleAxis,
+			divisor: 8,
+			labelInterpolationFnc: function(value) {
+				return moment(value).format('MMM D, HH:00');
+			},
+			showGrid: false
+		},
+		axisY: {
+			onlyInteger: true,
+			showGrid: false
+		}
+	};
+
+	return {
+		create: function() {
+			new Chartist.Line('#chart-timeline', data, options);
+		},
+		destroy: function() {
+		}
+	};
 };
-
-$(document).ready(function() {
-	$('select').material_select();
-	$('#modal-create-locale .btn-save').click(function() {
-		$('#form-locale').attr('action', $('#form-locale').attr('action') + '#locale=' + $('#field-locale-name').val());
-		$('#form-locale').submit();
-	});
-	$('#modal-create-key .btn-save').click(function() {
-		$('#form-key').attr('action', $('#form-key').attr('action') + '#key=' + $('#field-key-name').val());
-		$('#form-key').submit();
-	});
-
-	new Chartist.Line('#chart-timeline', data, options);
-});
 
 App.Modules.SuggestionModule = function(sb) {
 	function _handleSuggestionSelected(suggestion) {
@@ -51,3 +46,16 @@ App.Modules.SuggestionModule = function(sb) {
 
 App.Core.register('SuggestionModule', App.Modules.SuggestionModule);
 App.Core.register('ProjectSearchModule', App.Modules.ProjectSearchModule);
+App.Core.register('TimelineModule', App.Modules.TimelineModule);
+
+$(document).ready(function() {
+	$('select').material_select();
+	$('#modal-create-locale .btn-save').click(function() {
+		$('#form-locale').attr('action', $('#form-locale').attr('action') + '#locale=' + $('#field-locale-name').val());
+		$('#form-locale').submit();
+	});
+	$('#modal-create-key .btn-save').click(function() {
+		$('#form-key').attr('action', $('#form-key').attr('action') + '#key=' + $('#field-key-name').val());
+		$('#form-key').submit();
+	});
+});
