81c0ff857548dae2520cc5bae41b94caeb70073c
René Samselnig <Rene.Samselnig@skiline.cc>
2016-09-20 21:46:46 +0200

    Fix: Refactor dashboard JavaScript (addresses #11)

--- translatr/public/javascripts/dashboard.js
+++ translatr/public/javascripts/dashboard.js
@@ -1,16 +1,66 @@
-function search(value) {
-    if(value !== '') {
-		$('.card.project').hide();
-		$('.card.project[name*="' + value.toLowerCase() +'"]').show();
-    } else {
-    	$('.card.project').show();
-    }
-}
-$(document).ready(function() {
-	$('#field-search').on('change keyup paste', function() {
-		search($('#field-search').val());
-	});
-	$('.btn-save').click(function() {
-		$('#form-project').submit();
-	});
-});
+App.Modules.DashboardSearchModule = function(sb) {
+	var fieldSearch = sb.dom.find('#field-search');
+	var win = sb.dom.wrap(window);
+
+	function _handleSelect(suggestion) {
+		sb.publish('suggestionSelected', suggestion);
+	}
+
+	function _handleKeyPress(event) {
+		console.log(event);
+		if (event.which == 70 && (event.ctrlKey || event.metaKey)) {
+			event.preventDefault();
+			fieldSearch.focus().select();
+	    }
+	}
+
+	// TODO: Replace with AJAX search
+	function _handleSearch(value) {
+	    if(value !== '') {
+			$('.card.project').hide();
+			$('.card.project[name*="' + value.toLowerCase() +'"]').show();
+	    } else {
+	    	$('.card.project').show();
+	    }
+	}
+
+	return {
+		create: function() {
+			win.keydown(_handleKeyPress);
+
+			// TODO: Replace with AJAX search
+			fieldSearch.on('change keyup paste', function() {
+				_handleSearch(fieldSearch.val());
+			});
+
+//			fieldSearch.autocomplete({
+//				serviceUrl: jsRoutes.controllers.Application.dashboardSearch(projectId).url,
+//				onSelect: _handleSelect,
+//				triggerSelectOnValidInput: false,
+//				deferRequestBy: 200,
+//				paramName: 'search',
+//				groupBy: 'type'
+//			});
+		},
+		destroy: function() {
+		}
+	};
+};
+
+App.Modules.ProjectCreateModule = function(sb) {
+	var form = sb.dom.find('#form-project');
+	var buttonSave = sb.dom.find('#modal-create-project .btn-save');
+
+	return {
+		create: function() {
+			buttonSave.click(function() {
+				form.submit();
+			});
+		},
+		destroy: function() {
+		}
+	};
+};
+
+App.Core.register('DashboardSearchModule', App.Modules.DashboardSearchModule);
+App.Core.register('ProjectCreateModule', App.Modules.ProjectCreateModule);
