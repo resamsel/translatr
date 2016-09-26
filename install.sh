69622e10c66750286e2e2073f46193d2554cedd0
René Samselnig <Rene.Samselnig@skiline.cc>
2016-09-26 21:25:00 +0200

    Chg: Simplify install script

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 # Install location may be overridden by using the TRANSLATR_PREFIX env variable
 TRANSLATR_PREFIX=${TRANSLATR_PREFIX:=/usr/local}
+TRANSLATR=${TRANSLATR_PREFIX}/bin/translatr
 
-curl -fsSL https://raw.githubusercontent.com/resamsel/translatr/master/translatr/src/python/translatr.py > $TRANSLATR_PREFIX/bin/translatr
+curl -fsSL https://raw.githubusercontent.com/resamsel/translatr/master/translatr/src/python/translatr.py > ${TRANSLATR}
 
-chmod +x $TRANSLATR_PREFIX/bin/translatr
+chmod +x ${TRANSLATR}
 
-echo $TRANSLATR_PREFIX/bin/translatr created and made executable
+echo ${TRANSLATR} created and made executable
 
-$TRANSLATR_PREFIX/bin/translatr -h
+${TRANSLATR} -h
