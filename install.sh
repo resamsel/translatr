f88516fc1fd48d58147905758ebb6666481bf1e2
René Panzar <rene.samselnig@gmail.com>
2017-06-17 14:48:01 +0200

    Fix: Allow installation on Debian based systems

--- install.sh
+++ install.sh
@@ -7,13 +7,23 @@ TRANSLATR_TAG=${TRANSLATR_TAG:=master}
 
 TRANSLATR=${TRANSLATR_PREFIX}/bin/translatr
 
-curl -fsSL https://raw.githubusercontent.com/resamsel/translatr/${TRANSLATR_TAG}/src/python/translatr.py > ${TRANSLATR}
+curl -fsSL https://raw.githubusercontent.com/resamsel/translatr/${TRANSLATR_TAG}/src/python/translatr.py > /tmp/translatr.py
+
+if [ -w "$(basename ${TRANSLATR})" ]; then
+	mv /tmp/translatr.py ${TRANSLATR}
+else
+	echo "sudo required for installing ${TRANSLATR}..."
+	sudo mv /tmp/translatr.py ${TRANSLATR}
+fi
 
 chmod +x ${TRANSLATR}
 
 echo ${TRANSLATR} created and made executable
 
 if [ "$(which pip)" = "" ]; then
+	if [ "$(which easy_install)" = "" ]; then
+		sudo apt install python-setuptools
+	fi
 	echo "sudo required for installing PIP..."
 	sudo easy_install pip
 fi
