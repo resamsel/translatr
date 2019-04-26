d075a4ca28bd0748246b23da80ccce2bb2fb1c54
René Panzar <rene.samselnig@gmail.com>
2019-04-26 21:29:14 +0200

    Fix compile error.

--- app/validators/LocaleKeyCheck.java
+++ app/validators/LocaleKeyCheck.java
@@ -1,12 +1,13 @@
 package validators;
 
+import play.data.Form;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import javax.validation.Constraint;
-import javax.validation.Payload;
-import play.data.Form;
 
 /**
  *
@@ -18,8 +19,6 @@ import play.data.Form;
 @Constraint(validatedBy = LocaleKeyValidator.class)
 @Form.Display(name = "constraint.localekeyinvalid")
 public @interface LocaleKeyCheck {
-  String message() default LocaleKeyValidator.MESSAGE;
-
   Class<?>[] groups() default {};
 
   Class<? extends Payload>[] payload() default {};
