611899b791e19449d184b3bd6b91492e9b53a892
René Panzar <rene.panzar@gmail.com>
2019-05-13 13:51:27 +0200

    Adapt activity list.

--- ui/apps/translatr/src/app/modules/shared/activity-list/activity-list.module.ts
+++ ui/apps/translatr/src/app/modules/shared/activity-list/activity-list.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { MatIconModule, MatListModule } from "@angular/material";
-import { MomentModule } from "ngx-moment";
-import { GravatarModule } from "ngx-gravatar";
-import { ActivityListComponent } from "./activity-list.component";
-import { RouterModule } from "@angular/router";
-import { ActivityContentModule } from "../activity-content/activity-content.module";
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatChipsModule, MatIconModule, MatListModule} from '@angular/material';
+import {MomentModule} from 'ngx-moment';
+import {GravatarModule} from 'ngx-gravatar';
+import {ActivityListComponent} from './activity-list.component';
+import {RouterModule} from '@angular/router';
+import {ActivityContentModule} from '../activity-content/activity-content.module';
+import {TagModule} from '@translatr/translatr-components/src/lib/modules/tag/tag.module';
 
 @NgModule({
   declarations: [ActivityListComponent],
   imports: [
     CommonModule,
+    RouterModule,
+
     MatListModule,
     MatIconModule,
+    MatChipsModule,
+
     MomentModule,
     GravatarModule,
-    RouterModule,
-    ActivityContentModule
+
+    ActivityContentModule,
+    TagModule
   ],
   exports: [ActivityListComponent]
 })
---
+++ ui/libs/translatr-components/src/lib/modules/tag/tag.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-tag',
+  template: '<ng-content></ng-content>',
+  styles: [`:host {
+    background-color: rgba(0, 0, 0, 0.08);
+    border-radius: 4px;
+    padding: 4px;
+    font-weight: 500;
+  }`]
+})
+export class TagComponent {
+}
