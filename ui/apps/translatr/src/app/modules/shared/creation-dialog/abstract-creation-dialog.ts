3e30c464b73fb945d31b72ecdd18c755e4752888
René Panzar <rene.samselnig@gmail.com>
2019-07-17 23:09:59 +0200

    Refactor LocaleCreationDialogComponent and pull everything out into a separate base class

---
+++ ui/apps/translatr/src/app/modules/shared/creation-dialog/abstract-creation-dialog.ts
@@ -0,0 +1,70 @@
+import { Observable } from 'rxjs';
+import { MatDialogRef, MatSnackBar } from '@angular/material';
+import { take } from 'rxjs/operators';
+import { ConstraintViolation, Error, Locale } from '@dev/translatr-model';
+import { HostListener } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+const ENTER_KEYCODE = 'Enter';
+
+export abstract class AbstractCreationDialog<T, R> {
+
+  processing = false;
+  log = console.log;
+
+  constructor(
+    protected readonly snackBar: MatSnackBar,
+    protected readonly dialogRef: MatDialogRef<T, R>,
+    protected readonly form: FormGroup,
+    protected readonly creator: (r: R) => Observable<R>,
+    protected readonly messageProvider: (r: R) => string
+  ) {
+  }
+
+  public onSave(): void {
+    this.processing = true;
+
+    this.create()
+      .pipe(take(1))
+      .subscribe(
+        (r: R) => this.onCreated(r),
+        (res: { error: Error }) => this.onError(res.error)
+      );
+  }
+
+  protected isValid(): boolean {
+    return this.form.valid;
+  }
+
+  protected create(): Observable<R> {
+    return this.creator(this.form.value);
+  }
+
+  protected onCreated(r: R): void {
+    this.processing = false;
+    this.dialogRef.close(r);
+    this.snackBar.open(
+      this.messageProvider(r),
+      'Dismiss',
+      { duration: 3000 }
+    );
+  }
+
+  protected onError(error: Error): void {
+    this.processing = false;
+    error.error.violations.forEach((violation: ConstraintViolation) => {
+      const control = this.form.get(violation.field);
+      if (!!control) {
+        control.setErrors({ violation: violation.message });
+        control.markAsTouched();
+      }
+    });
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  protected onHotkey(event: KeyboardEvent) {
+    if (event.key === ENTER_KEYCODE && this.isValid() && !this.processing) {
+      this.onSave();
+    }
+  }
+}
--- ui/apps/translatr/src/app/modules/shared/locale-creation-dialog/locale-creation-dialog.component.ts
+++ ui/apps/translatr/src/app/modules/shared/locale-creation-dialog/locale-creation-dialog.component.ts
@@ -1,11 +1,9 @@
-import { Component, ElementRef, HostListener, Inject, ViewChild } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
-import { ConstraintViolation, Error, Locale } from '@dev/translatr-model';
+import { Locale } from '@dev/translatr-model';
 import { LocaleService } from '@dev/translatr-sdk';
-import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
-import { take } from 'rxjs/operators';
-
-const ENTER_KEYCODE = 'Enter';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractCreationDialog } from '../creation-dialog/abstract-creation-dialog';
 
 export const openLocaleCreationDialog = (dialog: MatDialog, projectId: string) => {
   return dialog.open
@@ -14,72 +12,33 @@ export const openLocaleCreationDialog = (dialog: MatDialog, projectId: string) =
 };
 
 @Component({
-  templateUrl: './locale-creation-dialog.component.html',
+  templateUrl: './locale-creation-dialog.component.html'
 })
-export class LocaleCreationDialogComponent {
+export class LocaleCreationDialogComponent
+  extends AbstractCreationDialog<LocaleCreationDialogComponent, Locale> {
+
   public get nameFormControl() {
     return this.form.get('name');
   }
 
   constructor(
-    private readonly snackBar: MatSnackBar,
-    private readonly localeService: LocaleService,
-    private readonly dialogRef: MatDialogRef<LocaleCreationDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) private readonly data: { projectId: string }
+    readonly snackBar: MatSnackBar,
+    readonly dialogRef: MatDialogRef<LocaleCreationDialogComponent, Locale>,
+    readonly localeService: LocaleService,
+    @Inject(MAT_DIALOG_DATA) readonly data: { projectId: string }
   ) {
-  }
-
-  @ViewChild('name') nameField: ElementRef;
-
-  form = new FormGroup({
-    projectId: new FormControl(this.data.projectId),
-    name: new FormControl('', [
-      Validators.required,
-      Validators.pattern('[^\\s/]+')
-    ])
-  });
-  public processing = false;
-
-  log = console.log;
-
-  public onSave(): void {
-    this.processing = true;
-    this.localeService
-      .create(this.form.value)
-      .pipe(take(1))
-      .subscribe(
-        (locale: Locale) => this.onCreated(locale),
-        (res: { error: Error }) => {
-          this.processing = false;
-
-          this.nameFormControl.setErrors(
-            res.error.error.violations.reduce(
-              (prev: ValidationErrors, violation: ConstraintViolation) => ({
-                ...prev,
-                [violation.field]: violation.message
-              }),
-              {}
-            )
-          );
-          this.nameFormControl.markAsTouched();
-        }
-      );
-  }
-
-  private onCreated(locale: Locale): void {
-    this.processing = false;
-    this.dialogRef.close(locale);
-    this.snackBar.open(
-      `Locale ${locale.name} has been created`,
-      'Dismiss',
-      { duration: 3000 }
+    super(
+      snackBar,
+      dialogRef,
+      new FormGroup({
+        projectId: new FormControl(data.projectId),
+        name: new FormControl('', [
+          Validators.required,
+          Validators.pattern('[^\\s/]+')
+        ])
+      }),
+      (locale: Locale) => localeService.create(locale),
+      (locale: Locale) => `Locale ${locale.name} has been created`
     );
-  }
-
-  @HostListener('window:keyup', ['$event'])
-  public onHotkey(event: KeyboardEvent) {
-    if (event.key === ENTER_KEYCODE && this.form.valid && !this.processing) {
-      this.onSave();
-    }
   }
 }
