e3ecc813c61b33e60026d08cdb10ef7a75a246b4
René Panzar <rene.samselnig@gmail.com>
2019-09-30 19:07:44 +0200

    Access Token: Edit form adaptions
    Fixes missing space between buttons.

--- ui/apps/translatr/src/app/modules/pages/user-page/user-access-token/user-access-token.component.ts
+++ ui/apps/translatr/src/app/modules/pages/user-page/user-access-token/user-access-token.component.ts
@@ -4,12 +4,6 @@ import { UserFacade } from '../+state/user.facade';
 import { filter, take } from 'rxjs/operators';
 import { AccessToken } from '@dev/translatr-model';
 
-const distinct = <T>(value: T, index: number, self: Array<T>) =>
-  self.indexOf(value) === index;
-
-const scopeType = scope => scope.split(':')[1];
-const scopePermission = scope => scope.split(':')[0];
-
 @Component({
   selector: 'app-user-access-token',
   templateUrl: './user-access-token.component.html',
