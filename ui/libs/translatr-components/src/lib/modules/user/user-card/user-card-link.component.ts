20d76058cdfa0ac2166cdc90cd32baee725eb6f7
René Panzar <rene.panzar@gmail.com>
2020-01-20 12:18:12 +0100

    Clean-up CSS and components.

--- ui/libs/translatr-components/src/lib/modules/user/user-card/user-card-link.component.ts
+++ ui/libs/translatr-components/src/lib/modules/user/user-card/user-card-link.component.ts
@@ -3,8 +3,7 @@ import { User } from '@dev/translatr-model';
 
 @Component({
   selector: 'app-user-card-link',
-  templateUrl: './user-card-link.component.html',
-  styleUrls: ['./user-card-link.component.scss']
+  templateUrl: './user-card-link.component.html'
 })
 export class UserCardLinkComponent implements OnInit {
   @Input() user: User;
