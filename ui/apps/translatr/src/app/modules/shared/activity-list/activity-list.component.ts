d10a3d870f7109214c51ca3963532f85a2db5e3a
René Panzar <rene.panzar@gmail.com>
2020-02-21 20:55:34 +0100

    Introduce improved activity list.

--- ui/apps/translatr/src/app/modules/shared/activity-list/activity-list.component.ts
+++ ui/apps/translatr/src/app/modules/shared/activity-list/activity-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
 import { Activity, PagedList } from '@dev/translatr-model';
 import { FilterCriteria } from '../list-header/list-header.component';
 
@@ -16,11 +16,39 @@ export class ActivityListComponent {
   @Output() filter = new EventEmitter<FilterCriteria>();
   @Output() more = new EventEmitter<number>();
 
+  @HostBinding('class') clazz = 'activity-list';
+
   trackByFn(index, item: Activity): string {
     return item.id;
   }
 
   onFilter(criteria: FilterCriteria): void {
     this.filter.emit(criteria);
+  }
+
+  iconType(contentType: string) {
+    switch (contentType) {
+      case 'dto.ProjectUser':
+        return 'member';
+      default:
+        return contentType.substr(contentType.indexOf('.') + 1).toLowerCase();
+    }
+  }
+
+  icon(contentType: string) {
+    switch (contentType) {
+      case 'dto.Project':
+        return 'view_quilt';
+      case 'dto.Locale':
+        return 'language';
+      case 'dto.Key':
+        return 'vpn_key';
+      case 'dto.Message':
+        return 'message';
+      case 'dto.ProjectUser':
+        return 'group';
+      default:
+        return 'vpn_key';
+    }
   }
 }
