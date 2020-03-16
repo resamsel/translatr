ae06d1978a5ea59bf705da7230059424811d82de
René Panzar <rene.panzar@gmail.com>
2020-03-16 20:22:08 +0100

    Introduce UI Unit Tests - addresses #125
    Adds tests for project activity and project info.

--- ui/apps/translatr/src/app/modules/pages/project-page/project-activity/project-activity.component.spec.ts
+++ ui/apps/translatr/src/app/modules/pages/project-page/project-activity/project-activity.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProjectActivityComponent } from './project-activity.component';
+import { ProjectFacade } from '../+state/project.facade';
+import { mockObservable } from '@translatr/utils/testing';
+import { ActivityListTestingModule } from '../../../shared/activity-list/testing';
 
 describe('ProjectActivityComponent', () => {
   let component: ProjectActivityComponent;
@@ -8,7 +10,18 @@ describe('ProjectActivityComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ProjectActivityComponent]
+      declarations: [ProjectActivityComponent],
+      imports: [ActivityListTestingModule],
+      providers: [
+        {
+          provide: ProjectFacade,
+          useFactory: () => ({
+            activitiesCriteria$: mockObservable(),
+            project$: mockObservable(),
+            unload$: mockObservable()
+          })
+        }
+      ]
     }).compileComponents();
   }));
 
--- ui/apps/translatr/src/app/modules/pages/project-page/project-info/project-info.component.spec.ts
+++ ui/apps/translatr/src/app/modules/pages/project-page/project-info/project-info.component.spec.ts
@@ -1,6 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectInfoComponent } from './project-info.component';
+import { ProjectFacade } from '../+state/project.facade';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatListModule,
+  MatProgressBarModule,
+  MatSelectModule,
+  MatTooltipModule
+} from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { WINDOW } from '@translatr/utils';
+import { FeatureFlagTestingModule, MetricTestingModule } from '@translatr/components/testing';
+import { NavListTestingModule } from '../../../shared/nav-list/testing';
+import { ActivityListTestingModule } from '../../../shared/activity-list/testing';
+import { ShortNumberModule } from '@dev/translatr-components';
+import { mockObservable } from '@translatr/utils/testing';
 
 describe('ProjectInfoComponent', () => {
   let component: ProjectInfoComponent;
@@ -8,7 +27,39 @@ describe('ProjectInfoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ProjectInfoComponent]
+      declarations: [ProjectInfoComponent],
+      imports: [
+        MetricTestingModule,
+        NavListTestingModule,
+        ActivityListTestingModule,
+        ShortNumberModule,
+        FeatureFlagTestingModule,
+
+        RouterTestingModule,
+
+        MatDialogModule,
+        MatCardModule,
+        MatListModule,
+        MatIconModule,
+        MatProgressBarModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatTooltipModule
+      ],
+      providers: [
+        {
+          provide: ProjectFacade,
+          useFactory: () => ({
+            project$: mockObservable(),
+            locales$: mockObservable(),
+            keys$: mockObservable(),
+            messages$: mockObservable(),
+            activities$: mockObservable()
+          })
+        },
+        { provide: WINDOW, useValue: { location: {} } }
+      ]
     }).compileComponents();
   }));
 
