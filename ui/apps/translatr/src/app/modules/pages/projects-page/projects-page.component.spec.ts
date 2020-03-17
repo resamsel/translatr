b8e22d37734e590828701cc087dc0a70eb3a92a0
René Panzar <rene.panzar@gmail.com>
2020-03-17 16:48:22 +0100

    Introduce UI Unit Tests - addresses #125
    Introduce tests for projects page.

--- ui/apps/translatr/src/app/modules/pages/projects-page/projects-page.component.spec.ts
+++ ui/apps/translatr/src/app/modules/pages/projects-page/projects-page.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProjectsPageComponent } from './projects-page.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ProjectsFacade } from './+state/projects.facade';
+import { AppFacade } from '../../../+state/app.facade';
+import { MatButtonModule, MatIconModule, MatTooltipModule } from '@angular/material';
+import { mockObservable } from '@translatr/utils/testing';
+import { FeatureFlagTestingModule } from '@translatr/components/testing';
+import { ProjectListTestingModule, SidenavTestingModule } from '../../testing';
+import { MatDialog } from '@angular/material/dialog';
 
 describe('UsersPageComponent', () => {
   let component: ProjectsPageComponent;
@@ -8,7 +15,34 @@ describe('UsersPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ProjectsPageComponent]
+      declarations: [ProjectsPageComponent],
+      imports: [
+        SidenavTestingModule,
+        FeatureFlagTestingModule,
+        ProjectListTestingModule,
+
+        RouterTestingModule,
+
+        MatButtonModule,
+        MatTooltipModule,
+        MatIconModule
+      ],
+      providers: [
+        {
+          provide: ProjectsFacade,
+          useFactory: () => ({
+            unload$: mockObservable()
+          })
+        },
+        {
+          provide: AppFacade,
+          useFactory: () => ({
+            me$: mockObservable(),
+            queryParams$: mockObservable()
+          })
+        },
+        { provide: MatDialog, useFactory: () => ({}) }
+      ]
     }).compileComponents();
   }));
 
--- ui/apps/translatr/src/app/modules/shared/testing.ts
+++ ui/apps/translatr/src/app/modules/shared/testing.ts
@@ -1,3 +1,4 @@
 export * from './activity-list/testing';
 export * from './nav-list/testing';
 export * from './project-card-list/testing';
+export * from './project-list/testing';
