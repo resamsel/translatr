f7635e77500d0c50d38f245543ec494fae812f85
René Panzar <rene.panzar@gmail.com>
2020-03-17 14:08:26 +0100

    Introduce UI Unit Tests - addresses #125
    Adds tests for project settings.

--- ui/apps/translatr/src/app/modules/pages/project-page/project-settings/project-settings.component.spec.ts
+++ ui/apps/translatr/src/app/modules/pages/project-page/project-settings/project-settings.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectSettingsComponent } from './project-settings.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatSnackBar } from '@angular/material';
+import { ProjectFacade } from '../+state/project.facade';
+import { ProjectService } from '@dev/translatr-sdk';
+import { RouterTestingModule } from '@angular/router/testing';
+import { mockObservable } from '@translatr/utils/testing';
+import { EmptyViewTestingModule } from '@translatr/components/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ProjectSettingsComponent', () => {
   let component: ProjectSettingsComponent;
@@ -8,7 +16,31 @@ describe('ProjectSettingsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ProjectSettingsComponent]
+      declarations: [ProjectSettingsComponent],
+      imports: [
+        EmptyViewTestingModule,
+
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        NoopAnimationsModule,
+
+        MatDialogModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule
+
+      ],
+      providers: [
+        { provide: MatSnackBar, useFactory: () => ({}) },
+        {
+          provide: ProjectFacade,
+          useFactory: () => ({
+            project$: mockObservable()
+          })
+        },
+        { provide: ProjectService, useFactory: () => ({}) }
+      ]
     })
       .compileComponents();
   }));
