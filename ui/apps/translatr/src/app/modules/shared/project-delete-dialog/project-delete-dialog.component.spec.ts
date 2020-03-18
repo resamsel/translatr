88a2a2b43b84e09eb5b1d26e822bdc0c0e7435e9
René Panzar <rene.panzar@gmail.com>
2020-03-18 17:10:59 +0100

    Introduce UI Unit Tests - addresses #125
    Introduce tests for the project delete dialog module.

--- ui/apps/translatr/src/app/modules/shared/project-delete-dialog/project-delete-dialog.component.spec.ts
+++ ui/apps/translatr/src/app/modules/shared/project-delete-dialog/project-delete-dialog.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectDeleteDialogComponent } from './project-delete-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef, MatFormFieldModule, MatInputModule, MatSnackBar } from '@angular/material';
+import { ProjectService } from '@dev/translatr-sdk';
 
 describe('ProjectDeleteDialogComponent', () => {
   let component: ProjectDeleteDialogComponent;
@@ -8,7 +12,21 @@ describe('ProjectDeleteDialogComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ProjectDeleteDialogComponent]
+      declarations: [ProjectDeleteDialogComponent],
+      imports: [
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+
+        MatDialogModule,
+        MatFormFieldModule,
+        MatInputModule
+      ],
+      providers: [
+        { provide: MatSnackBar, useValue: {} },
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: ProjectService, useFactory: () => ({}) }
+      ]
     })
       .compileComponents();
   }));
