2a54dcab0b1938d057a88f357db01af5e0816ad6
René Panzar <rene.panzar@gmail.com>
2020-03-18 18:01:25 +0100

    Introduce UI Unit Tests - addresses #125 and #153
    Introduce tests for the project owner edit form module.

--- ui/apps/translatr/src/app/modules/shared/project-owner-edit-form/project-owner-edit-form.component.spec.ts
+++ ui/apps/translatr/src/app/modules/shared/project-owner-edit-form/project-owner-edit-form.component.spec.ts
@@ -1,6 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectOwnerEditFormComponent } from './project-owner-edit-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MAT_DIALOG_DATA,
+  MatAutocompleteModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatSnackBar
+} from '@angular/material';
+import { ChangeDetectorRef } from '@angular/core';
+import { ProjectService } from '@dev/translatr-sdk';
 
 describe('ProjectMemberEditFormComponent', () => {
   let component: ProjectOwnerEditFormComponent;
@@ -8,7 +20,22 @@ describe('ProjectMemberEditFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectOwnerEditFormComponent ]
+      declarations: [ProjectOwnerEditFormComponent],
+      imports: [
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatAutocompleteModule
+      ],
+      providers: [
+        { provide: MatSnackBar, useValue: {} },
+        { provide: ProjectService, useFactory: () => ({}) },
+        { provide: ChangeDetectorRef, useFactory: () => ({}) },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
     })
     .compileComponents();
   }));
