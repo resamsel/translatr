215532e77b8972861efb299eaf643b0192d5e206
René Panzar <rene.panzar@gmail.com>
2020-06-16 07:55:40 +0200

    Key Editor: Add language creation teaser - addresses #137
    Adds e2e test that verifies the creation of the language.

--- ui/apps/translatr-e2e/src/integration/project/keys/project-key-editor-empty.spec.ts
+++ ui/apps/translatr-e2e/src/integration/project/keys/project-key-editor-empty.spec.ts
@@ -13,7 +13,6 @@ describe('Project Key Editor Empty State', () => {
     cy.route('/api/johndoe/p0*', 'fixture:johndoe/p0');
     cy.route('/api/johndoe/p0/keys/k1', 'fixture:johndoe/p0/keys/k1');
     cy.route('/api/project/*/locales*', 'fixture:johndoe/p0/locales');
-    cy.route('/api/project/*/locales?*missing=true*', 'fixture:johndoe/p0/locales-missing');
     cy.route('/api/project/*/messages*', 'fixture:johndoe/p0/messages');
     cy.route('/api/project/*/members*', 'fixture:johndoe/p0/members');
     cy.route('/api/project/*/activities*', 'fixture:johndoe/p0/activities');
@@ -52,5 +51,25 @@ describe('Project Key Editor Empty State', () => {
 
     // then
     cy.get('mat-dialog-container').should('exist');
+  });
+
+  it('should show language creation dialog when clicking button', () => {
+    // given
+
+    // when
+    page.navigateTo();
+    cy.get('dev-empty-view [data-test="create-locale"]').click();
+    cy.get('mat-dialog-container input').type('de');
+    cy.route('POST', '/api/locale', 'fixture:johndoe/p0/locale-created');
+    cy.route('/api/project/*/locales*', 'fixture:johndoe/p0/locales-added');
+    cy.get('mat-dialog-container button.save').click();
+
+    // then
+    page
+      .getNavList()
+      .find('.mat-list-item.active')
+      .should('have.length', 1)
+      .find('h3.mat-line')
+      .should('have.text', 'German');
   });
 });
