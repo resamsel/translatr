fc9588700f5878c5c154719b42eccf2e28a83075
René Panzar <rene.panzar@gmail.com>
2020-06-29 17:40:29 +0200

    Introduce git commit and tag of the release

--- src/typescript/release.ts
+++ src/typescript/release.ts
@@ -1,5 +1,6 @@
 import { promises } from "fs";
 import { inc, ReleaseType } from "semver";
+import simpleGit from "simple-git";
 
 const updateJson = (filename: string, version: string): Promise<string> => {
   return promises
@@ -43,10 +44,68 @@ const release = (releaseType: ReleaseType): Promise<string> => {
     .then(version => updateSbt("build.sbt", version));
 };
 
+const gitCheck = (): Promise<void> => {
+  return simpleGit(".")
+    .status()
+    .then(result => {
+      if (!result.isClean()) {
+        throw new Error("workspace contains uncommitted changes");
+      }
+    });
+};
+
+/**
+ * Creates a Git commit with the bumped versions included.
+ */
+const gitCommit = (version: string): Promise<string> => {
+  const git = simpleGit();
+  return git
+    .add(".")
+    .then(() => git.commit(`Bump version to v${version}`))
+    .then(() => version);
+};
+
+/**
+ * Creates a Git tag for the current commit.
+ */
+const gitTag = (version: string): Promise<string> => {
+  return simpleGit()
+    .addTag(`v${version}`)
+    .then(() => version);
+};
+
+/**
+ * Rebases current branch onto given.
+ */
+const gitRebase = (version: string, target: string): Promise<string> => {
+  return simpleGit()
+    .rebase([target])
+    .then(() => version);
+};
+
 if (process.argv.length == 3) {
-  release(process.argv[2] as ReleaseType).then(version =>
-    console.log(`Version has been bumped to ${version}`)
-  );
+  gitCheck()
+    .then(() => release(process.argv[2] as ReleaseType))
+    .then(version => {
+      console.log(`Version has been bumped to ${version}`);
+      return version;
+    })
+    .then(version => gitCommit(version))
+    .then(version => {
+      console.log(`Changes have been committed`);
+      return version;
+    })
+    .then(version => gitTag(version))
+    .then(version => {
+      console.log(`Commit has been tagged with v${version}`);
+      return version;
+    })
+    .then(version => gitRebase(version, "master"))
+    .then(version => {
+      console.log(`Branch has been rebased onto master v${version}`);
+      return version;
+    })
+    .catch(error => console.error(`${error}`));
 } else {
   console.error("No release type specified.");
 }
