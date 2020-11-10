1968310f0c503423958aaf95526b4140da992064
René Panzar <rene.panzar@gmail.com>
2020-11-10 07:27:53 +0100

    Deactivate stream module for now

--- app/services/NotificationService.java
+++ app/services/NotificationService.java
@@ -1,15 +1,18 @@
 package services;
 
+import com.google.inject.ImplementedBy;
 import criterias.NotificationCriteria;
 import io.getstream.client.exception.StreamClientException;
 import io.getstream.client.model.activities.AggregatedActivity;
 import io.getstream.client.model.activities.SimpleActivity;
 import io.getstream.client.model.beans.StreamResponse;
 import models.ActionType;
+import services.impl.NotificationServiceDummy;
 
 import java.io.IOException;
 import java.util.UUID;
 
+@ImplementedBy(NotificationServiceDummy.class)
 public interface NotificationService {
   boolean isEnabled();
 
