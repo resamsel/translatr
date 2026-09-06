11e8d413232e9391d63e4afc31a900ce5951e653
René Panzar <rene.panzar@gmail.com>
2026-09-06 22:11:33 +0200

    test(openapi): extend the OpenAPI merge guard to cover AuthClientsResource

--- src/test/java/com/translatr/controller/OpenApiMergeTest.java
+++ src/test/java/com/translatr/controller/OpenApiMergeTest.java
@@ -56,6 +56,9 @@ class OpenApiMergeTest {
             .body("paths['/api/health'].get.responses.200.description", is("The service is healthy."))
             .body("paths['/api/member/{id}'].get.responses.200.description", is("The member."))
             .body("paths", org.hamcrest.Matchers.hasKey("/api/statistics"))
-            .body("paths['/api/statistics'].get.responses.200.description", is("Aggregate counts across the whole instance."));
+            .body("paths['/api/statistics'].get.responses.200.description", is("Aggregate counts across the whole instance."))
+            .body("paths['/api/member/{id}'].get.responses.200.description", is("The member."))
+            .body("paths", org.hamcrest.Matchers.hasKey("/api/authclients"))
+            .body("paths['/api/authclients'].get.responses.200.description", is("Auth providers a visitor can actually use."));
     }
 }
