fcf68643c1fe2a85d2a7ddeea29bf67399007296
René Panzar <rene.panzar@gmail.com>
2026-09-06 22:40:24 +0200

    build(openapi): generate NotificationDto, PagedNotificationList and NotificationsApi

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,7 +99,8 @@ openApiGenerate {
     // (missing class/module), not as an obvious openapi-generator failure.
     globalProperties.set(
         mapOf(
-            "models" to "OidcProviderStatus,AccessTokenPayload,PageMetadata,PagedAccessTokenList,ProjectPayload,PagedProjectList,MemberDto,PagedMemberList,MessagePayload,PagedMessageList,LocaleDto,PagedLocaleList,KeyDto,PagedKeyList,GlobalFeatureFlagDto,ResolvedFeatureDto,HealthStatus,StatisticsDto,AuthClientDto",
+            "models" to "OidcProviderStatus,AccessTokenPayload,PageMetadata,PagedAccessTokenList,ProjectPayload,PagedProjectList,MemberDto,PagedMemberList,MessagePayload,PagedMessageList,LocaleDto,PagedLocaleList,KeyDto,PagedKeyList,GlobalFeatureFlagDto,ResolvedFeatureDto,HealthStatus,StatisticsDto,AuthClientDto,NotificationDto,PagedNotificationList",
+            "models" to "OidcProviderStatus,AccessTokenPayload,PageMetadata,PagedAccessTokenList,ProjectPayload,PagedProjectList,MemberDto,PagedMemberList,MessagePayload,PagedMessageList,LocaleDto,PagedLocaleList,KeyDto,PagedKeyList",
             "apis" to "",
             "supportingFiles" to ""
         )
