cde2c03e2b806006d9a150e813608c3b48b033c4
René Panzar <rene.panzar@gmail.com>
2026-09-06 22:03:22 +0200

    build(openapi): generate AuthClientDto and its API interface

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,7 +99,7 @@ openApiGenerate {
     // (missing class/module), not as an obvious openapi-generator failure.
     globalProperties.set(
         mapOf(
-            "models" to "OidcProviderStatus,AccessTokenPayload,PageMetadata,PagedAccessTokenList,ProjectPayload,PagedProjectList,MemberDto,PagedMemberList,MessagePayload,PagedMessageList,LocaleDto,PagedLocaleList,KeyDto,PagedKeyList,GlobalFeatureFlagDto,ResolvedFeatureDto,HealthStatus,StatisticsDto",
+            "models" to "OidcProviderStatus,AccessTokenPayload,PageMetadata,PagedAccessTokenList,ProjectPayload,PagedProjectList,MemberDto,PagedMemberList,MessagePayload,PagedMessageList,LocaleDto,PagedLocaleList,KeyDto,PagedKeyList,GlobalFeatureFlagDto,ResolvedFeatureDto,HealthStatus,StatisticsDto,AuthClientDto",
             "apis" to "",
             "supportingFiles" to ""
         )
